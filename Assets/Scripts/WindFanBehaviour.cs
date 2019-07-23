9882e4afa7b4fd94571a79534aec97d71d9f2bcd
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-07-23 16:17:40 -0500

    added script for wind fan

---
+++ Assets/Scripts/WindFanBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class WindFanBehaviour : MonoBehaviour
+{
+    public bool change_velocity = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        float lift_duration = 10;
+        if (other.CompareTag("Ball"))
+        {
+            other.GetComponent<Rigidbody>();
+            other.attachedRigidbody.AddForce(new Vector3(200,0,0), ForceMode.Acceleration);
+            other.attachedRigidbody.velocity = new Vector3(8, 8, 8) * Time.deltaTime;
+            //change_velocity = true;
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Ball"))
+    //    {
+    //        other.GetComponent<Rigidbody>();
+    //        other.attachedRigidbody.AddForce(new Vector3(-350, 0,0), ForceMode.Force);
+    //    }
+    //}
+}
