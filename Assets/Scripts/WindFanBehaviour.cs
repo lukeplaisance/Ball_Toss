9227ab89c0056bbd6baccc487adb14dca47879de
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-07-25 11:52:38 -0500

    now uses a constant force for wind

--- Assets/Scripts/WindFanBehaviour.cs
+++ Assets/Scripts/WindFanBehaviour.cs
@@ -2,29 +2,22 @@
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WindFanBehaviour : MonoBehaviour
 {
-    public bool change_velocity = false;
+    public ConstantForce cf;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        float lift_duration = 10;
-        if (other.CompareTag("Ball"))
-        {
-            other.GetComponent<Rigidbody>();
-            other.attachedRigidbody.AddForce(new Vector3(200,0,0), ForceMode.Acceleration);
-            other.attachedRigidbody.velocity = new Vector3(8, 8, 8) * Time.deltaTime;
-            //change_velocity = true;
-        }
+        other.attachedRigidbody.GetComponent<ConstantForce>().enabled = true;
+        cf.force = new Vector3(20,0,0);
     }
 
     //private void OnTriggerExit(Collider other)
     //{
-    //    if (other.CompareTag("Ball"))
-    //    {
-    //        other.GetComponent<Rigidbody>();
-    //        other.attachedRigidbody.AddForce(new Vector3(-350, 0,0), ForceMode.Force);
-    //    }
+    //    other.attachedRigidbody.GetComponent<ConstantForce>();
+    //    cf.force = new Vector3(-20, -5, 0);
     //}
 }
